1217ae08e2635c13cee82aed7a66b5febfd26952
akash911133 <akashughade77@gmail.com>
2026-02-22 17:06:21 +0530

    update the changes

--- aws-infra-modules/ec2/internal-alb-asg.tf
+++ aws-infra-modules/ec2/internal-alb-asg.tf
@@ -167,11 +167,12 @@ resource "aws_launch_template" "backend_lt" {
     security_groups             = [aws_security_group.backend_sg.id]
   }
 
-  user_data = base64encode(templatefile("${path.module}/../userdata/backend.sh", {
-    S3_BUCKET_NAME = var.s3_bucket_name
-    CLIENT_NAME = var.client_name
-    CLIENT_ENVIRONMENT = var.client_environment
-  }))
+  user_data = templatefile("${path.module}/../userdata/backend.sh", {
+    S3_BUCKET_NAME      = var.s3_bucket_name
+    CLIENT_NAME         = var.client_name
+    CLIENT_ENVIRONMENT  = var.client_environment
+    AWS_REGION          = var.aws_region
+  })
 }
 
 ############################
--- aws-infra-modules/ec2/public-alb-asg.tf
+++ aws-infra-modules/ec2/public-alb-asg.tf
@@ -93,11 +93,12 @@ resource "aws_launch_template" "frontend_lt" {
     security_groups             = [aws_security_group.frontend_sg.id]
   }
 
-  user_data = base64encode(templatefile("${path.module}/../userdata/frontend.sh", {
-    S3_BUCKET_NAME = var.s3_bucket_name
-    CLIENT_NAME = var.client_name
-    CLIENT_ENVIRONMENT = var.client_environment
-  }))
+  user_data = templatefile("${path.module}/../userdata/frontend.sh", {
+    S3_BUCKET_NAME      = var.s3_bucket_name
+    CLIENT_NAME         = var.client_name
+    CLIENT_ENVIRONMENT  = var.client_environment
+    AWS_REGION          = var.aws_region
+  })
 }
 
 
